74fbac5461e63bba7ef234caada69cb840097d11
Bamlak Amedie <bamedie@css.edu>
2024-10-25 19:44:11 -0500

    Done with the Learning activity.

--- Unit8_SRP_F24/TradeProcessor.cs
+++ Unit8_SRP_F24/TradeProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System;
 
 namespace SingleResponsibilityPrinciple
 {
@@ -128,9 +129,9 @@ namespace SingleResponsibilityPrinciple
             string datadirConnectString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;";
             // This users the Azure connection string
             string azureConnectString = @"Data Source=cis3115-server.database.windows.net;Initial Catalog=CIS3115;User ID=cis3115;Password=Saints4SQL;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-           
+            string bamlakConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ""C:\Users\Bamlak Amedie\Documents\tradedatabase.mdf""; Integrated Security = True; Connect Timeout = 30";
             // Change the connection string used to match the one you want
-            using (var connection = new SqlConnection(genericConnectString))
+            using (var connection = new SqlConnection(bamlakConnectionString))
             {
                 LogMessage("INFO:Going to open database connection");
                 connection.Open();
